16068d010afa500ade15fda1f59ddd15f949ca01
DouradoCtrl <gdouradoestudante@gmail.com>
2026-05-06 09:27:39 -0300

    fix: trocado convensão de middleware por proxy

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,8 @@
 import { withAuth } from "next-auth/middleware";
 import { NextRequest } from "next/server";
 
-export const middleware = withAuth(
-  function middleware(req: NextRequest) {
+export const proxy = withAuth(
+  function proxy(req: NextRequest) {
     return;
   },
   {
