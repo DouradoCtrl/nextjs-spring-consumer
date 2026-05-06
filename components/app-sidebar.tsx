cd25a2c3ffcc4338a303a4ce103fc16015dde466
DouradoCtrl <gdouradoestudante@gmail.com>
2026-05-06 15:56:16 -0300

    chore: remove next link

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 
-import Link from "next/link";
 import { NavMain } from "@/components/nav-main";
 import { NavUser } from "@/components/nav-user";
 import { SidebarHeaderLogo } from "@/components/SidebarHeaderLogo";
