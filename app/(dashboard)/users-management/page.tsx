79ddccf764af77390fec688c253f89d8da8c9203
DouradoCtrl <gdouradoestudante@gmail.com>
2026-05-07 14:12:07 -0300

    feat(dashboard): realizado consulta da api em /api/users/all e exibido em um tabela

--- app/(dashboard)/users-management/page.tsx
+++ app/(dashboard)/users-management/page.tsx
@@ -1,18 +1,79 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { ShieldCheck, ShieldClose, Pencil, Trash2 } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
+  const { data: session } = useSession();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const token = session?.accessToken;
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/users/all`;
+
+    if (token) {
+      fetch(url, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((res) => res.json())
+        .then(setUsers);
+    }
+  }, [session]);
+
   return (
-    <>
-      {/* 
-          O <header> e o <SidebarInset> já estão no layout.
-          Aqui colocamos apenas o que é único desta página.
-      */}
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+    <div className="p-4">
+      <div className="rounded-md border overflow-hidden">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">#</TableHead>
+              <TableHead>Nome</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Usuário</TableHead>
+              <TableHead>Nível de Acesso</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Ações</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users.map((user: any, index: number) => (
+              <TableRow key={user.id}>
+                <TableCell>
+                  {/*  Fazer contador*/}
+                  { index + 1}
+                </TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.role == "ADMIN" ? "Administrador" : "Usuário" }</TableCell>
+                <TableCell>
+                  {user.enabled ? <ShieldCheck className="h-5 w-5" /> : <ShieldClose className="h-5 w-5" />}
+                </TableCell>
+                <TableCell>
+                  <Button>
+                    <Pencil className="h-5 w-5"/>
+                  </Button>
+                  <Button>
+                    <Trash2 className="h-5 w-5"/>
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
-    </>
+    </div>
   );
 }
