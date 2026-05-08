8f7dee4d523fafbbdcc69133e0fe51eba0f7feb2
DouradoCtrl <gdouradoestudante@gmail.com>
2026-05-08 16:12:20 -0300

    feat(dashboard): implementado header em todas as páginas

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { DynamicBreadcrumbs } from "@/components/dynamic-breadcrumbs";
-import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function DashboardLayout({
   children,
@@ -16,14 +10,7 @@ export default function DashboardLayout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        {/* faça */}
-        <header className="flex items-center p-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2" />
-          <DynamicBreadcrumbs />
-        </header>
-
-        <main className="flex flex-1 flex-col gap-4 p-4">{children}</main>
+        {children}
       </SidebarInset>
     </SidebarProvider>
   );
--- app/(dashboard)/users-management/page.tsx
+++ app/(dashboard)/users-management/page.tsx
@@ -22,6 +22,7 @@ import {
   ShieldClose,
   Trash2,
 } from "lucide-react";
+import {Header} from "@/components/header";
 
 interface User {
   id: string;
@@ -47,62 +48,63 @@ export default function Page() {
   }, [session?.accessToken]); // 3. Usar uma dependência mais específica
 
   return (
-    <div className="p-4">
-      <div className="rounded-md border overflow-hidden">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="text-center">#</TableHead>
-              <TableHead>Nome</TableHead>
-              <TableHead>Email</TableHead>
-              <TableHead>Usuário</TableHead>
-              <TableHead className="text-center">Administrador</TableHead>
-              <TableHead className="text-center">Status</TableHead>
-              <TableHead className="text-center">Ações</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {users.map((user, index) => (
-              <TableRow key={user.id}>
-                <TableCell className="text-center">{index + 1}</TableCell>
-                <TableCell>{user.name}</TableCell>
-                <TableCell>{user.email}</TableCell>
-                <TableCell>
-                  <Badge>{user.username}</Badge>
-                </TableCell>
-                <TableCell className="text-center">
-                  <div className="flex justify-center">
-                    {user.role === "ADMIN" ? (
-                      <ShieldCheck className="h-5 w-5 text-green-500" />
-                    ) : (
-                      <ShieldClose className="h-5 w-5 text-red-500" />
-                    )}
-                  </div>
-                </TableCell>
-                <TableCell className="text-center">
-                  <div className="flex justify-center">
-                    {user.enabled ? (
-                      <CircleCheck className="h-5 w-5 text-green-500" />
-                    ) : (
-                      <CircleX className="h-5 w-5 text-red-500" />
-                    )}
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <div className="flex justify-center gap-2">
-                    <Button size="icon" variant="outline">
-                      <Pencil className="h-4 w-4" />
-                    </Button>
-                    <Button size="icon" variant="destructive">
-                      <Trash2 className="h-4 w-4" />
-                    </Button>
-                  </div>
-                </TableCell>
+      <div className="flex flex-1 flex-col">
+        <Header />
+        <div className="flex flex-1 flex-col p-4">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="text-center">#</TableHead>
+                <TableHead>Nome</TableHead>
+                <TableHead>Email</TableHead>
+                <TableHead>Usuário</TableHead>
+                <TableHead className="text-center">Administrador</TableHead>
+                <TableHead className="text-center">Status</TableHead>
+                <TableHead className="text-center">Ações</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+            </TableHeader>
+            <TableBody>
+              {users.map((user, index) => (
+                <TableRow key={user.id}>
+                  <TableCell className="text-center">{index + 1}</TableCell>
+                  <TableCell>{user.name}</TableCell>
+                  <TableCell>{user.email}</TableCell>
+                  <TableCell>
+                    <Badge>{user.username}</Badge>
+                  </TableCell>
+                  <TableCell className="text-center">
+                    <div className="flex justify-center">
+                      {user.role === "ADMIN" ? (
+                        <ShieldCheck className="h-5 w-5 text-green-500" />
+                      ) : (
+                        <ShieldClose className="h-5 w-5 text-red-500" />
+                      )}
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-center">
+                    <div className="flex justify-center">
+                      {user.enabled ? (
+                        <CircleCheck className="h-5 w-5 text-green-500" />
+                      ) : (
+                        <CircleX className="h-5 w-5 text-red-500" />
+                      )}
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex justify-center gap-2">
+                      <Button size="icon" variant="outline">
+                        <Pencil className="h-4 w-4" />
+                      </Button>
+                      <Button size="icon" variant="destructive">
+                        <Trash2 className="h-4 w-4" />
+                      </Button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
     </div>
   );
 }
